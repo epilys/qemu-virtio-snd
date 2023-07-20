48f9b776f5b90c560b8731ed96747e5429e781ec
Emmanouil Pitsidianakis <manos.pitsidianakis@linaro.org>
2023-07-20 14:07:04 +0300

    virtio-sound: handle VIRTIO_SND_PCM_SET_PARAMS
    Handle the set parameters control request. It reconfigures a stream
    based on a guest's preference if the values are valid and supported.
    
    Signed-off-by: Emmanouil Pitsidianakis <manos.pitsidianakis@linaro.org>

--- hw/virtio/virtio-snd.c
+++ hw/virtio/virtio-snd.c
@@ -277,6 +277,30 @@ uint32_t virtio_snd_pcm_set_params_impl(VirtIOSound *s,
 }
 
 /*
+ * Handles the VIRTIO_SND_R_PCM_SET_PARAMS request.
+ *
+ * @s: VirtIOSound device
+ * @cmd: The request command queue element from VirtIOSound cmdq field
+ */
+static void virtio_snd_handle_pcm_set_params(VirtIOSound *s,
+                                             virtio_snd_ctrl_command *cmd)
+{
+    virtio_snd_pcm_set_params req;
+    size_t sz = iov_to_buf(cmd->elem->out_sg,
+                           cmd->elem->out_num,
+                           0,
+                           &req,
+                           sizeof(req));
+    if (sz != sizeof(virtio_snd_pcm_set_params)) {
+        cmd->resp.code = VIRTIO_SND_S_BAD_MSG;
+        return;
+    }
+
+    trace_virtio_snd_handle_pcm_set_params(req.hdr.stream_id);
+    cmd->resp.code = virtio_snd_pcm_set_params_impl(s, &req);
+}
+
+/*
  * Get a QEMU Audiosystem compatible format value from a VIRTIO_SND_PCM_FMT_*
  */
 static AudioFormat virtio_snd_get_qemu_format(uint32_t format)
@@ -477,6 +501,8 @@ process_cmd(VirtIOSound *s, virtio_snd_ctrl_command *cmd)
         virtio_snd_handle_pcm_start_stop(s, cmd, false);
         break;
     case VIRTIO_SND_R_PCM_SET_PARAMS:
+        virtio_snd_handle_pcm_set_params(s, cmd);
+        break;
     case VIRTIO_SND_R_PCM_PREPARE:
     case VIRTIO_SND_R_PCM_RELEASE:
         cmd->resp.code = VIRTIO_SND_S_NOT_SUPP;
